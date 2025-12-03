02f55e86855abfda984f7c1786558f9cdba808f6
Alex chow <zhouchuanglin@gmail.com>
2025-12-03 04:49:48 +0100

    完善 API demo

--- MPDemoConsole/ProductUpdateDto.cs
+++ MPDemoConsole/ProductUpdateDto.cs
@@ -9,7 +9,7 @@ using System.Text.Json.Serialization;
 
 namespace DensenMP;
 
-public partial class ProductsApi
+public partial class ProductUpdateDto
 {
 
     [DisplayName("ID")]
@@ -23,9 +23,6 @@ public partial class ProductsApi
 
     [DisplayName("名称")]
     public string? ProductName { get; set; }
-
-    [DisplayName("库存")]
-    public double? UnitsInStock { get; set; } = 0;
 
     [DisplayName("进价")]
     public decimal? PricePurchase { get; set; } = 0;
@@ -60,22 +57,6 @@ public partial class ProductsApi
     [DisplayName("折扣")]
     public int Discount { get; set; }
 
-    [DisplayName("数量")]
-    public int Quantity { get => quantity ?? Math.Max(1, QuantityPerUnit); set => quantity = value; }
-    private int? quantity;
-
-    [DisplayName("名称")]
-    public string? ProductNameIn { get; set; }
-
-    [DisplayName("售价")]
-    public int? UnitPriceIn { get; set; }
-
-    [DisplayName("折扣")]
-    public int? DiscountIn { get; set; }
-
-    [DisplayName("数量")]
-    public int? Inventory { get; set; }
-
     /// <summary>
     /// 小包装
     /// </summary>
@@ -90,39 +71,4 @@ public partial class ProductsApi
     [JsonPropertyOrder(101)]
     public int QuantityPerUnit2nd { get; set; } = 1;
 
-    /// <summary>
-    /// 进货量
-    /// </summary>
-    [DisplayName("进货量")]
-    public double? UnitsOnPurchase { get; set; } = 0;
-
-    [JsonConverter(typeof(JsonStringEnumConverter))]
-    [DisplayName("状态")]
-    [JsonPropertyOrder(102)]
-    public ProductStatus MarketingNO { get; set; } = ProductStatus.Normal;
-
-    /// <summary>
-    /// 编辑日期
-    /// </summary>
-    [DisplayName("编辑日期")]
-    [JsonPropertyOrder(6)]
-    public string? LastModify { get; set; }
-
-
-    [JsonPropertyOrder(7)]
-    [DisplayName("存放位置")]
-    public string? StorageLocation { get; set; }
-}
-public enum ProductStatus
-{
-    Normal = 0,
-    Reserve = 1,
-    Discontinued = 2,
-    OutOfStock = 3,
-    Generally = 4,
-    SpecialOffer = 5,
-    Promotion = 6,
-    HotSell = 7,
-    NewProducts = 8,
-    NotSet = -1,
 }
--- MPDemoConsole/Program.cs
+++ MPDemoConsole/Program.cs
@@ -4,17 +4,29 @@
 // e-mail:zhouchuanglin@gmail.com 
 // **********************************
 
+using Microsoft.Extensions.Configuration;
 using System.Net.Http.Json;
+using System.Reflection;
+
+var configuration = new ConfigurationBuilder()
+    .AddUserSecrets(Assembly.GetExecutingAssembly(), optional: true) // 可选：如果没有用户机密，不会抛出异常
+    .Build();
 
 //1. 联系客服获取
 var densenID = "123456789XCXUQ14mg==";
+
+var endpoint = "https://mp.app1.es";
+
+// 也可以从用户机密读取 densenID
+densenID = configuration["DensenID"] ?? "123456789XCXUQ14mg==";
+endpoint = configuration["DensenEndpoint"] ?? "https://mp.app1.es";
 
 var barcode = "8437008795515";
 
 var client = new HttpClient();
 
 //2. 获取 api token
-var densenResult = await client.GetFromJsonAsync<DensenMP.ResponseToken>($"https://mp.app1.es/api/getProduct/{densenID}");
+var densenResult = await client.GetFromJsonAsync<DensenMP.ResponseToken>($"{endpoint}/api/getProduct/{densenID}");
 
 if (densenResult == null || string.IsNullOrEmpty(densenResult.Token.ToString()))
 {
@@ -25,24 +37,87 @@ if (densenResult == null || string.IsNullOrEmpty(densenResult.Token.ToString()))
 
 //使用token获取商品资料, 缓存5分钟. 意思是5分钟内都是同一份数据.降低客户数据主机压力
 
-//https://mp.app1.es/api/getProduct/{densenID}/{token}
+//{endpoint}/api/getProduct/{densenID}/{token}
 
-
-//4. 查询商品
-
-//https://mp.app1.es/api/getProduct/{densenID}/{token}/{barcode}
-
-var response = await client.GetFromJsonAsync<DensenMP.ResponseProduct>($"https://mp.app1.es/api/getProduct/{densenID}/{densenResult.Token}/{barcode}");
+var response = await client.GetFromJsonAsync<DensenMP.ResponseProduct>($"{endpoint}/api/getProduct/{densenID}/{densenResult.Token}");
 if (response?.Items != null && response.Items.Count >= 0)
 {
     // 处理响应数据
+    Console.WriteLine($"Product List:\r\n{"UserCode",-8}\t{"BarCode",-15}\t{"ProductName",-15}\t{"UnitPrice",8}");
+    foreach (var item in response.Items.Take(10))
+    {
+        Console.WriteLine($"{item.UserCode,-8}\t{item.BarCode,-15}\t{item.ProductName,-15}\t{item.UnitPrice:n2}");
+    }
+}
+else
+{
+    Console.WriteLine("No items found or invalid response.");
+}
+await Task.Delay(1000);
+
+
+//4.添加商品 Post
+
+//{endpoint}/api/addProduct/{densenID}/{int:token}
+//FromBody ProductUpdateDto
+
+var newProduct = new DensenMP.ProductUpdateDto
+{
+    UserCode = "TEST001",
+    BarCode = barcode,
+    ProductName = "测试商品",
+    UnitPrice = 9.99M,
+    UnitPrice2 = 8.99M,
+};
+var postResponse = await client.PostAsJsonAsync($"{endpoint}/api/addProduct/{densenID}/{densenResult.Token}", newProduct);
+var postResult = await postResponse.Content.ReadAsStringAsync();
+Console.WriteLine($"\r\n\r\nAdd Product Response: {postResult}");
+await Task.Delay(1000);
+
+
+//5.编辑商品 Post
+
+//{endpoint}/api/updateProduct/{densenID}/{int:token}
+//FromBody ProductUpdateDto
+
+var updateProduct = new DensenMP.ProductUpdateDto
+{
+    UserCode = "TEST001",
+    BarCode = barcode,
+    ProductName = "测试商品-更新",
+    UnitPrice = 10.99M,
+    UnitPrice2 = 9.99M,
+    UnitPrice3 = 8.99M,
+    UnitPrice8 = 5.99M,
+};
+
+var updateResponse = await client.PostAsJsonAsync($"{endpoint}/api/updateProduct/{densenID}/{densenResult.Token}", updateProduct);
+var updateResult = await updateResponse.Content.ReadAsStringAsync();
+Console.WriteLine($"\r\n\r\nUpdate Product Response: {updateResult}");
+await Task.Delay(1000);
+
+//6. 查询商品
+
+//{endpoint}/api/getProduct/{densenID}/{token}/{barcode}
+
+response = await client.GetFromJsonAsync<DensenMP.ResponseProduct>($"{endpoint}/api/getProduct/{densenID}/{densenResult.Token}/{barcode}");
+if (response?.Items != null && response.Items.Count >= 0)
+{
+    // 处理响应数据
+    Console.WriteLine($"\r\n\r\nProduct:");
     foreach (var item in response.Items)
     {
         Console.WriteLine($"Product UserCode: {item.UserCode}");
         Console.WriteLine($"Product BarCode: {item.BarCode}");
         Console.WriteLine($"Product Name: {item.ProductName}");
         Console.WriteLine($"Product Price: {item.UnitPrice}");
-
+        Console.WriteLine($"Product Price2: {item.UnitPrice2}");
+        Console.WriteLine($"Product Price3: {item.UnitPrice3}");
+        Console.WriteLine($"Product Price4: {item.UnitPrice4}");
+        Console.WriteLine($"Product Price5: {item.UnitPrice5}");
+        Console.WriteLine($"Product Price6: {item.UnitPrice6}");
+        Console.WriteLine($"Product Price7: {item.UnitPrice7}");
+        Console.WriteLine($"Product Price8: {item.UnitPrice8}");
     }
 }
 else
@@ -50,6 +125,7 @@ else
     Console.WriteLine("No items found or invalid response.");
 }
 
-//5.盘点库存
+//7.盘点库存 Get
 
-//https://mp.app1.es/api/changeQuantity/{densenID}/{int:token}/{barcode}/{int:quantity}
+//{endpoint}/api/changeQuantity/{densenID}/{int:token}/{barcode}/{int:quantity}
+
